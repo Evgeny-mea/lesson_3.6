ad60dc488fc2fca247c95ffdf734a96f8fe0ce52
Evgeny-mea <70842210+Evgeny-mea@users.noreply.github.com>
2021-02-09 21:05:04 +0700

    DZ_3.6

--- src/MainArr.java
+++ src/MainArr.java
@@ -36,6 +36,7 @@ public class MainArr {
         return newArr;
     }
 
+
     public static boolean Arr2(int[] arr){
         for (int i = 0; i < arr.length; i++) {
             if(arr[i] != 1 && arr[i] != 4) return false;
